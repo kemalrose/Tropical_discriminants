dba94058c73892ed9496d988c72d32a294022aed
kemalrose <30701894+kemalrose@users.noreply.github.com>
2021-12-13 11:50:07 +0100

    FIX WIHT SIMON

--- example_discriminant.jl
+++ example_discriminant.jl
@@ -2,12 +2,18 @@ using Oscar
 using LinearAlgebra
 
 A = [0 1 0 1 2 1; 0 0 1 1 1 2; 1 1 1 1 1 1]
+
+A = [1 1 1 1 1 1;
+0 1 2 0 1 0;
+0 0 0 1 1 2]
+
 n, m = size(A)
 S = MatrixSpace(ZZ, n, m)
 B = nullspace(S(A))[2]
 S,U,V = snf_with_transform(B)
 Π = Matrix(U[1:m-n, :])
 U_inv  = Matrix(inv(U))
+Ξ = U_inv[:,1:m-n]
 
 A = Matrix{Int64}(A)
 B = Matrix{Int64}(B)
@@ -26,9 +32,9 @@ matroid = Polymake.matroid.Matroid(VECTORS = B)
 
 function getV(w)
     #println(w)
-    w_new = U_inv * [w; zeros(Int,n)]
+    w_new = Π'*w
+    w_new  = w_new * 1000 + rand(1:10, size(w_new))
     vtx, flag = getVertex2(w_new, Γ, dets, RR, Fσ)
-    println("FLAG : $flag")
     Π * (vtx - v_0)
 end
 w_new  = [1 1 1 1 1 1]'
@@ -43,6 +49,10 @@ vtcs = get_Polytope(V_0, getV);
 length(vtcs)
 length(unique!(vtcs))
 P = convex_hull(hcat(vtcs...)')
+vertices(P)
+
+
+
 
 
 rand_verts  = sampleRandom(Γ, dets, RR, Fσ, 1000)
@@ -51,7 +61,7 @@ l_points  = lattice_points(P)
 verts = vertices(P)
 
 rand_verts_proj = [Π*(v-rand_verts[1]) for v in rand_verts]
-rand_verts_proj = [Π*(v-v_0) for v in rand_verts]
+#rand_verts_proj = [Π*(v-v_0) for v in rand_verts]
 P_proj = convex_hull( (hcat(rand_verts_proj...)'))
 l_points_proj  = lattice_points(P_proj)
 verts_proj = vertices(P_proj)
@@ -74,11 +84,97 @@ function getV(u)
 end
 
 
+
+
+
+
+
+
+
+function getV_proj(u)
+    verts  = [Vector{Int64}(v) for v in vertices(P_proj)]
+    inner_prod  = [ (u' * v) for v in verts ]
+    verts[argmax(inner_prod)]
+end
+
+function getV(u)
+    verts  = [Vector{Int64}(v) for v in vertices(P)]
+    inner_prod  = [ (u' * v) for v in verts ]
+    verts[argmax(inner_prod)]
+end
+
+function adapted_getV(u)
+     u_new = Π' * u
+     #u_new  = 100*u_new + rand(1:10, size(u_new))
+     vert = getV(u_new)
+     Π * (vert - rand_verts[1])
+end
+
+
+
+
+
+
+
+function getV(w)
+    w_new = Π'*w
+    w_new  = w_new * 10000 + rand(-100:100, size(w_new))
+    vtx, flag = getVertex2(-w_new, Γ, dets, RR, Fσ)
+    println("w : $w")
+    println("w_new : $w_new")
+    println("flag : $flag")
+    Π * (vtx - v_0)
+end
+
+v_0 = rand_verts[1]
+V_0 = rand_verts_proj[1:5]
 V_old, F_old   = [], []
 V_new  = V_0
 V_old = V_new
-V_new, F_new = update(V_old, F_old, getV)
+V_new, F_new = update(V_old, F_old, getV_proj)
+
+verts, fcts, P = get_Polytope(V_0, getV)
+
+L_points  = lattice_points(P)
 
 
+function Horn_param(A, B, v, λ)
+    φλ = [prod(λ.^A[:,i]) for i in 1:n]
+    φλ.*(B*v)
+end
 
-get_Polytope(V_0, getV)
+
+function get_Vdm(pts, mons)
+    V = zeros(Complex{Float64}, (length(pts), length(mons)))
+    for i in 1:length(pts)
+        V[i, :] = [prod(pts[i].^m) for m in mons]
+        V[i, :] = V[i, :]/norm(V[i, :])
+    end
+    V
+end
+
+function interpolate_discr(A)
+    B =
+    P =
+    mons = lattice_points(P)
+    
+    n_pts = length(mons) * 2
+
+    V
+end
+pts = [Horn_param(A,B,randn(ComplexF64,n-d),randn(ComplexF64,d)) for j = 1:50]
+Π = Matrix{Int64}(Π)
+pts_proj = [[prod(p.^Π[i,:]) for i in 1:n-d] for p in pts]
+L_points = Vector{Int64}.(L_points)
+
+V = get_Vdm(pts_proj, L_points)
+nullspace(V)
+svd(V)
+
+x = pts[1]
+det([2*x[1] x[2] x[4]; x[2] 2*x[3] x[5]; x[4] x[5] 2*x[6]])
+
+for x in pts
+    println(abs(det([2*x[1] x[2] x[4]; x[2] 2*x[3] x[5]; x[4] x[5] 2*x[6]])
+))
+end
--- get_newton.jl
+++ get_newton.jl
@@ -6,14 +6,16 @@ function get_Polytope(V_0, getVtx)
     V_old, F_old = [], []
     V_new  = copy(V_0)
     F_new = []
+    P = convex_hull(hcat(V_0...)')
+    
     while size(V_old) != size(V_new)
         V_old = copy(V_new)
         F_old = copy(F_new)
         #println("in while loop")
         #println(V_old,F_old,getVtx)
-        V_new, F_new = update(V_old, F_old, getVtx)
+        V_new, F_new, P = update(V_old, F_old, getVtx)
     end
-    return copy(V_new)
+    return copy(V_new), F_new, P
 end
 
 function update(V_old, F_old, getVtx)
@@ -22,7 +24,11 @@ function update(V_old, F_old, getVtx)
     P = convex_hull(hcat(V_old...)')
     F_new  = [Vector{Int64}(f.a)  for f in facets(P)]
     keep_facets = []
-    #println(" setdiff: $(setdiff(F_new,F_old))")
+    println(" setdiff: $(setdiff(F_new,F_old))")
+    println(" F_new: $(F_new)")
+    println(" F_old: $(F_old)")
+
+
     for a in setdiff(F_new, F_old)
         vert = getVtx(a)
         if !in(vert, V_new)
@@ -33,12 +39,16 @@ function update(V_old, F_old, getVtx)
             V_new = push!(V_new, vert)
         else
             println("NO NEW VERTEX -> FOUND FACET")
+            println("------ V_old : $V_old")
+            println("------ vert : $vert")
             index = findfirst(ℓ -> ℓ == a, F_new)
             keep_facets = push!(keep_facets, index)
             println("keep_facets: $keep_facets")
         end
     end
     #println(V_new)
+    println("List of facets : $([copy(F_old); F_new[keep_facets]])")
     println("------- DONE UPDATING ")
-    copy(V_new), [copy(F_old); F_new[keep_facets]]
+
+    copy(V_new), [copy(F_old); F_new[keep_facets]], P
 end
