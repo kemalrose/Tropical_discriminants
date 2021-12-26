c825cbb2c9cdf72b2e1b7b4f43043eee592deffb
kemalrose <30701894+kemalrose@users.noreply.github.com>
2021-12-26 18:31:46 +0100

    Bernd's example is too large! Vdm matrix can not be constructed.

--- example_discriminant.jl
+++ example_discriminant.jl
@@ -1,5 +1,5 @@
-using Oscar
-using LinearAlgebra, HomotopyContinuation
+
+using LinearAlgebra, HomotopyContinuation, Oscar
 
 A = [0 1 0 1 2 1; 0 0 1 1 1 2; 1 1 1 1 1 1]
 
@@ -18,131 +18,35 @@ A = [1 1 1 1 1 1  1 1 1 1 1 1;
       0 0 0 1 1 2 0 0 0 1 1 2;
       0 1 2 0 1 0 0 1 2 0 1 0]
 
-
-V_0 = sampleRandom(Γ, dets, RR, Fσ, 1000)
-P = convex_hull(hcat(V_0...)')
-v_0, vtcs, fcts, Pol = newton_pol(A, B, Π)
-disc, B, coeff, mons, err_tol = interpolate_discr(A)
-
-verify(disc, mons, coeff, A, B; strategy = "interpolation")
+A =      [ 1 1 1 1 0 0 0 0;
+       0 0 0 0 1 1 1 1;
+       2 3 5 7 11 13 17 19;
+      19 17 13 11 7 5 3 2]
 
 
 
+data = data_from_matrix(A)
 
+disc, intcoeff, coeff, mons, err_tol = interpolate_discr(A)
 
+verify(disc, mons, coeff, data; strategy = "interpolation")
 
-d, n = size(A)
-S = MatrixSpace(ZZ, d, n)
-B = nullspace(S(A))[2]
-S,U,V = snf_with_transform(B)
-Π = Matrix{Int64}(U[1:n-d, :])
-A = Matrix{Int64}(A)
-B = Matrix{Int64}(B)
-U_inv  = Matrix{Int64}(inv(U))
-U = Matrix{Int64}(U)
-
-v_0, vtcs, fcts, Pol = newton_pol(A,B,Π)
-L_pts_proj = lattice_points(Pol)
-mons = [ U_inv * [v ; zeros(d)] + v_0 for v in Vector{Int64}.(L_pts_proj)]
-mons = [ [m.num for m in mon] for mon in mons]
-
-vtcs_proj = [ [m.num for m in mon] for mon in vtcs]
-verts_ambient = [ U_inv * [v ; zeros(d)] + v_0 for v in Vector{Int64}.(vtcs_proj)]
-P_ambient = convex_hull(hcat(verts_ambient...)')
-L_pts = lattice_points(P_ambient)
-
-
-indices = findall(l -> sum(l.<0)>0, verts_ambient )
-
-
-pts = [Horn_param(A,B,randn(ComplexF64, n-d),randn(ComplexF64,d)) for j = 1:length(mons)*2]
-
-V = get_Vdm(pts, mons)
-
-U, sings, VV = svd(V)
-coeff = VV[:, end]
-
-max_val  = maximum(abs.(coeff))
-coeff = coeff./ coeff[findfirst(k-> abs(coeff[k]) == max_val, 1:length(coeff))]
-coeff = real.(coeff)
-
-
-err_tol = sings[end]/sings[end-1] * 100
-coeff = rationalize.(coeff, tol = err_tol)
-
-@var a[1:n]
-monomials = [prod(a.^mon) for mon in mons]
+#nsamples = 20
+#n = data.n
+#for i = 1:nsamples
+#    w = rand(-10000:10000,n)
+#
+#    is_contained, is_in_face = cone_containments(w, data)
+#    non_genericity = sum(is_in_face.==1)
+#    println("is not generic: $non_genericity")
+#    #monomial = getVertex(w, data)
+#    #monomials = unique!(push!(monomials,monomial))
+#end
 
 
 
 
 
-
-
-d, n = size(A)
-
-matroid = Polymake.matroid.Matroid(VECTORS = B)
-ΣB = Polymake.tropical.matroid_fan{min}(matroid)
-#ΣB = Polymake.tropical.matroid_fan_from_flats{min}(matroid)
-Γ, dets, RR, Fσ = getConeData(A, ΣB)
-
-
-V_0 = sampleRandom(Γ, dets, RR, Fσ, 2(n - d))
-while Oscar.dim(convex_hull(hcat(V_0...)')) != n - d
-    V_0 = sampleRandom(Γ, dets, RR, Fσ, 4(n - d))
-end
-#V_0 = [ [vi.num for vi in v] for v in V_0 ]
-v_0 = V_0[1]
-V_0 = [Π*(v - v_0) for v in V_0]
-
-function getV(w)
-    w_new = Π'*w
-    w_new  = w_new * 10000 + rand(-100:100, size(w_new))
-    vtx, flag = getVertex2(-w_new, Γ, dets, RR, Fσ)
-    monomial = Π * (vtx - v_0)
-    if sum(vtx.<0)>0
-        println("Get negative exponents for weight w_new = $w_new.")
-        println("vtx = $vtx.")
-        println("w_proj = $w.")
-        println("monomial = $monomial.")
-    end
-    monomial
-end
-
-
-println("get_Polytope")
-V_old, F_old = [], []
-V_new  = copy(V_0)
-F_new = []
-P = convex_hull(hcat(V_0...)')
-
-while size(V_old) != size(V_new)
-    V_old = copy(V_new)
-    F_old = copy(F_new)
-    #println("in while loop")
-    #println(V_old,F_old,getVtx)
-    V_new, F_new, P = update(V_old, F_old, getVtx)
-end
-return copy(V_new), F_new, P
-
-
-
-V_new = copy(V_old)
-P = convex_hull(hcat(V_old...)')
-F_new  = [Vector{Int64}(f.a)  for f in facets(P)]
-keep_facets = []
-
-for a in setdiff(F_new, F_old)
-    vert = getVtx(a)
-    if !in(vert, V_new)
-    #if setdiff(V_old,vert) == V_old
-        V_new = push!(V_new, vert)
-    else
-
-        index = findfirst(ℓ -> ℓ == a, F_new)
-        keep_facets = push!(keep_facets, index)
-    end
-end
 #println(V_new)
 #rand_verts  = sampleRandom(Γ, dets, RR, Fσ, 1000)
 #P = convex_hull(  hcat(rand_verts...)'  )
--- get_newton.jl
+++ get_newton.jl
@@ -1,9 +1,9 @@
 
 using Oscar
 
-function Horn_param(A, B, v, λ)
-    φλ = [prod(λ.^A[:,i]) for i in 1:n]
-    φλ.*(B*v)
+function Horn_param(data, v, λ)
+    φλ = [prod(λ.^data.A[:,i]) for i in 1:n]
+    φλ.*(data.B*v)
 end
 
 function get_Vdm(pts, mons; normalize = true)
@@ -19,78 +19,28 @@ function get_Vdm(pts, mons; normalize = true)
 end
 
 
-function interpolate_discr(A)
-    d, n = size(A)
-    S = MatrixSpace(ZZ, d, n)
-    B = nullspace(S(A))[2]
-    S,U,V = snf_with_transform(B)
-    Π = Matrix{Int64}(U[1:n-d, :])
-    A = Matrix{Int64}(A)
-    B = Matrix{Int64}(B)
-    U_inv  = Matrix{Int64}(inv(U))
-
-
-    v_0, vtcs, fcts, Pol = newton_pol(A,B,Π)
-    L_pts_proj = lattice_points(Pol)
-    mons = [ U_inv * [v ; zeros(d)] + v_0 for v in Vector{Int64}.(L_pts_proj)]
-    mons = [ [m.num for m in mon] for mon in mons]
-
-    pts = [Horn_param(A,B,randn(ComplexF64, n-d),randn(ComplexF64,d)) for j = 1:length(mons)*2]
-
-    V = get_Vdm(pts, mons)
-
-    U, sings, VV = svd(V)
-    coeff = VV[:, end]
-
-    max_val  = maximum(abs.(coeff))
-    coeff = coeff./ coeff[findfirst(k-> abs(coeff[k]) == max_val, 1:length(coeff))]
-    coeff = real.(coeff)
-
-
-    err_tol = sings[end]/sings[end-1] * 100
-    coeff = rationalize.(coeff, tol = err_tol)
-
-    @var a[1:n]
-    monomials = [prod(a.^mon) for mon in mons]
-    (coeff'monomials)[1], B, coeff, mons, err_tol
-end
-
-
-
-function newton_pol(A, B, Π)
-    d, n = size(A)
-
-    matroid = Polymake.matroid.Matroid(VECTORS = B)
-    ΣB = Polymake.tropical.matroid_fan{min}(matroid)
-    #ΣB = Polymake.tropical.matroid_fan_from_flats{min}(matroid)
-    Γ, dets, RR, Fσ = getConeData(A, ΣB)
-
-
-    V_0 = sampleRandom(Γ, dets, RR, Fσ, 2(n - d))
-    while Oscar.dim(convex_hull(hcat(V_0...)')) != n - d
-        V_0 = sampleRandom(Γ, dets, RR, Fσ, 4(n - d))
+function newton_pol(data::Aux_data)
+    n, d = data.n, data.d
+    V_ambient = sampleRandom(data, 2(n - d))
+    while Oscar.dim(convex_hull(hcat(V_ambient...)')) != n - d
+        V_ambient = unique!([V_ambient; sampleRandom(data,(n - d))])
     end
-    #V_0 = [ [vi.num for vi in v] for v in V_0 ]
-    v_0 = V_0[1]
-    V_0 = [Π*(v - v_0) for v in V_0]
+    #V_ambient = [ [vi.num for vi in v] for v in V_ambient ]
+    v_0 = V_ambient[1]
+    V_0 = [data.Π*(v - v_0) for v in V_ambient]
 
     function getV(w)
-        w_new = Π'*w
+        w_new = data.Π'*w
         w_new  = w_new * 10000 + rand(-100:100, size(w_new))
-        vtx, flag = getVertex2(-w_new, Γ, dets, RR, Fσ)
-        monomial = Π * (vtx - v_0)
-        if sum(vtx.<0)>0
-            println("Get negative exponents for weight w_new = $w_new.")
-            println("vtx = $vtx.")
-            println("w_proj = $w.")
-            println("monomial = $monomial.")
-        end
+        vtx = getVertex(-w_new, data)
+        monomial = data.Π * (vtx - v_0)
         monomial
     end
     vtcs, fcts, Pol = get_Polytope(V_0, getV);
 
     v_0, vtcs, fcts, Pol
 end
+
 
 
 function get_Polytope(V_0, getVtx)
@@ -126,14 +76,13 @@ function update(V_old, F_old, getVtx)
             keep_facets = push!(keep_facets, index)
         end
     end
-    #println(V_new)
-
     copy(V_new), [copy(F_old); F_new[keep_facets]], P
 end
 
 
-function verify(disc, mons, coeff, A, B; strategy = "substitution")
-    d, n = size(A)
+function verify(disc, mons, coeff, data; strategy = "substitution")
+    A, B, d, n = data.A, data.B, data.d, data.n
+
     if strategy == "substitution"
         @var λ[1:d], v[1:n-d]
         φλ = [prod(λ.^A[:,i]) for i in 1:n]
@@ -152,4 +101,55 @@ function verify(disc, mons, coeff, A, B; strategy = "substitution")
     else
         print("Choose input strategy to either be substitution or interpolation")
     end
+end
+
+
+
+
+
+function interpolate_discr(A; interpolation_method = "SVD")
+    data = data_from_matrix(A)
+    n, d = data.n, data.d
+
+    println("1. Compute the Newton polytope P of the discriminant")
+    v_0, vtcs, fcts, Pol = newton_pol(data)
+
+    println("2. Find its lattice points")
+    L_pts_proj = lattice_points(Pol)
+
+    println("   P has $(length(L_pts_proj)) lattice points")
+    mons = [ data.Π_rinv * v + v_0 for v in Vector{Int64}.(L_pts_proj)]
+    mons = [ [m.num for m in mon] for mon in mons]
+
+    println("3. Construct the interpolation problem")
+    pts = [Horn_param(data,randn(ComplexF64, n-d),randn(ComplexF64,d)) for j = 1:convert(Int64,round(length(mons)*1.2))]
+    V = get_Vdm(pts, mons)
+    println("   Constructed a Vandermonde matrix of size $(size(V))")
+
+    println("4. Find floating point coefficients...")
+    if interpolation_method == "SVD"
+        println("   ... using SVD")
+        U, sings, VV = svd(V)
+        coeff = VV[:, end]
+        err_tol = sings[end]/sings[end-1] * 100
+    elseif interpolation_method == "eigs"
+        println("   ... using eigs")
+        VtV = V' * V
+        @time EVobj = eigs(VtV; nev = 2, which = :SM)
+        coeff = EVobj[2][:, 1]
+        println(EVobj[1])
+        err_tol = abs(EVobj[1][1]/EVobj[1][2]) * 100
+    else
+        println("Choose interpolation_method to be either SVD or eigs.")
+    end
+
+    println("5. Rationalizing with tolerance $(err_tol)")
+    nonzinds = findall(ℓ->abs(ℓ)>err_tol, coeff)
+    coeff = coeff/coeff[findfirst(ℓ->abs(ℓ) == minimum(abs.(coeff[nonzinds])),coeff)]
+    intcoeff = convert.(Int64,round.(real.(coeff)))
+    #coeff = rationalize.(coeff, tol = err_tol)
+
+    @var a[1:n]
+    monomials = [prod(a.^mon) for mon in mons]
+    (intcoeff'monomials)[1], intcoeff, coeff, mons, err_tol
 end
